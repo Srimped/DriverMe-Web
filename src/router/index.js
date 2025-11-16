0cdb210c8a7e731d0dd0bfdd6c6e9c8bf1d34f03
Srimped <nickey200202@gmail.com>
2025-11-16 19:04:18 +0700

    feat: adding login, vue-router
    add vue-router and adding login form 20%

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from '../views/Home.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: Home
+  },
+
+  {
+    path: '/login',
+    name: 'login',
+    component: () => import('../views/Login.vue')
+  },
+
+  {
+    path: '/dashboard',
+    name: 'dashboard',
+    component: () => import('../views/Dashboard.vue')
+  },
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="login_form">
+    <form action="">
+      <h4>Login</h4>
+      <div class="container">
+        <label for="username">Username</label>
+        <input type="text" name="username" placeholder="Your username"/>
+      </div>
+      <div class="container">
+        <label for="password">Password</label>
+        <input type="password" name="password" placeholder="Your password"/>
+      </div>
+      <button class="login_btn" type="submit">Login</button>
+    </form>
+    <a class="home_btn" href="/upload">Home page</a>
+  </div>
+</template>
+
+<script setup>
+
+</script>

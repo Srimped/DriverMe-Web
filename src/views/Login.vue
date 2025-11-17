8182edb6dae9939fd535f06ce8f45a2f3487a7c3
Srimped <nickey200202@gmail.com>
2025-11-17 22:02:10 +0700

    feat: Login form
    styling Login css adding some validation

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,21 +1,44 @@
 <template>
-  <div class="login_form">
-    <form action="">
-      <h4>Login</h4>
-      <div class="container">
-        <label for="username">Username</label>
-        <input type="text" name="username" placeholder="Your username"/>
-      </div>
-      <div class="container">
-        <label for="password">Password</label>
-        <input type="password" name="password" placeholder="Your password"/>
-      </div>
-      <button class="login_btn" type="submit">Login</button>
-    </form>
-    <a class="home_btn" href="/upload">Home page</a>
+  <div class="bg-stone-200">
+    <div class="mx-auto max-w-[600px]">
+      <form action="" @submit.prevent="auth" class="p-5 mt-50 mb-5 bg-white border border-stone-300 rounded-md">
+        <h4 class="text-center text-2xl font-medium">Login</h4>
+        <p class="text-red-500">{{ error }}</p>
+        <div class="flex flex-col">
+          <label class="text-stone-700" for="username">Username</label>
+          <input v-model="account.username" class="pl-2.5 pt-3 pb-3 rounded-md border-stone-200 border-[1px]" type="text" placeholder="Your username"/>
+        </div>
+        <div class="flex flex-col">
+          <label class="text-stone-700" for="password">Password</label>
+          <input v-model="account.password" class="pl-2.5 pt-3 pb-3 rounded-md border-stone-200 border-[1px]" type="password" placeholder="Your password"/>
+        </div>
+        <button @click="" class="w-full bg-cyan-500 mt-5 py-4 rounded-sm text-center font-bold text-white cursor-pointer hover:bg-cyan-600 transition" type="submit">Login</button>
+      </form>
+      <a class="py-1 px-4 text-black hover:bg-stone-300 hover:rounded-md transition" href="/">Home page</a>
+    </div>
   </div>
 </template>
 
 <script setup>
+import { reactive, ref, watch } from 'vue';
+
+const error = ref()
+const account = reactive({
+  username: '',
+  password: ''
+})
+
+console.log(error)
+
+const auth = (account, () => {
+  error.value = !account.username || !account.password
+  ? 'Please enter your account' : dashboard()
+
+  
+})
+
+const dashboard = () => {
+  
+}
 
 </script>
